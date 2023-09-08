fd0e004e00107c3f87d8c175b83a93cc90445e46
wuwei <wuw@tuguiyao-gd.com>
2023-09-08 15:22:07 +0800

    集群

---
+++ 集群/keep+nginx/nginx_check.sh
@@ -0,0 +1,10 @@
+# vi /etc/keepalived/nginx_check.sh
+#!/bin/bash
+A=`ps -C nginx –no-header |wc -l`
+if [ $A -eq 0 ];then
+/usr/local/nginx/sbin/nginx
+sleep 2
+if [ `ps -C nginx --no-header |wc -l` -eq 0 ];then
+	killall keepalived
+fi
+fi
