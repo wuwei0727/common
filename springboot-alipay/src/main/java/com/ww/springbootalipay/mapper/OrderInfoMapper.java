4d2e84527728fa83ad4b3be70ef3b0c7d796d026
wuwei0727 <lrr20030112@outlook.com>
2023-11-22 17:41:01 +0800

    11

--- springboot-alipay/src/main/java/com/ww/springbootalipay/mapper/OrderInfoMapper.java
+++
@@ -1,8 +0,0 @@
-package com.ww.springbootalipay.mapper;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ww.springbootalipay.dao.OrderInfo;
-
-public interface OrderInfoMapper extends BaseMapper<OrderInfo> {
-
-}
--- springboot-alipay/src/main/java/com/ww/springbootalipay/mapper/ProductMapper.java
+++
@@ -1,9 +0,0 @@
-package com.ww.springbootalipay.mapper;
-
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.ww.springbootalipay.dao.Product;
-
-public interface ProductMapper extends BaseMapper<Product> {
-
-}
